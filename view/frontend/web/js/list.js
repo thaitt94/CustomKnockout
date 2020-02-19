d533d98be29b8aa22c6bdbd7ad0593e51fe6752b
Daisy <59386062+thaitt94@users.noreply.github.com>
2020-02-19 21:11:58 +0700

    Update list.js

--- view/frontend/web/js/list.js
+++ view/frontend/web/js/list.js
@@ -4,25 +4,23 @@ define([
     'mage/validation',
     'ko',
     'Magento_Ui/js/modal/modal'
-    ], 
+    ],
     function ($, Component, validation, ko, modal) {
         'use strict';
         return Component.extend({
         	defaults: {
                 template: 'Thai_Knockout1/list'
             },
-            self = this,
             totalEmployee: ko.observableArray([]),
-            PerPage = ko.observable(),
-            Page = ko.observable(1),
             initialize: function (config) {
                 this._super();
                 if (config.employees.length > 0) {
                     this.totalEmployee(config.employees);
+                    return this;
                 }
             },
 
-            save: function (data) 
+            save: function (data)
             {
                 var employee = {},
                 self = this,
@@ -67,7 +65,7 @@ define([
                 return ind;
             },
 
-            remove: function (item) 
+            remove: function (item)
             {
                 var confirm_delete = confirm('Are you sure to delete ' + item.full_name + ' ?');
                 var self = this;
@@ -91,97 +89,58 @@ define([
                 }
             },
 
-            addEmployeePopup: function(object, event) {
+            createPopup: function(employee, event) {
                 var elmPopup = $("#employee-form-popup");
                 $( "#dob" ).datepicker();
-                var option = {
-                    'type': 'popup',
-                    'title': 'Add employee',
-                    'responsive': true,
-                    'buttons': [{
-                        text: 'Cancel',
-                        class: 'action',
-                        click: function () {
-                            $(elmPopup).find("input").val("");
-                            this.closeModal();
+        	    if (employee.entity_id == null) {
+                    let option = {
+                        'type': 'popup',
+                        'title': 'Add employee',
+                        'responsive': true,
+                        'buttons': [{
+                            text: 'Cancel',
+                            class: 'action',
+                            click: function () {
+                                $(elmPopup).find("input").val("");
+                                $(elmPopup).find("select").val("");
+                                this.closeModal();
+                            }
+                        }],
+                        closed: function () {
                         }
-                    }],
-                    closed: function () {
-                    }
-                };
-                modal(option, $(elmPopup));
-                $(elmPopup).find("input").val("");
-                $(elmPopup).find("select").val("");
-                $(elmPopup).modal("openModal");
-            },
-
-            editEmployee: function (employee, event) {
-                var elmPopup = $("#employee-form-popup");
-                var option = {
-                    'type': 'popup',
-                    'title': $.mage.__("Edit employee: %1").replace("%1", employee.full_name),
-                    'responsive': true,
-                    'buttons': [{
-                        text: 'Cancel',
-                        class: 'action',
-                        click: function () {
-                            $(elmPopup).find("input").val("");
-                            $(elmPopup).find("select").val("");
-                            this.closeModal();
+                    };
+                    modal(option, $(elmPopup));
+                    $(elmPopup).modal("openModal");
+                } else {
+                    var option = {
+                        'type': 'popup',
+                        'title': $.mage.__("Edit employee: %1").replace("%1", employee.full_name),
+                        'responsive': true,
+                        'buttons': [{
+                            text: 'Cancel',
+                            class: 'action',
+                            click: function () {
+                                $(elmPopup).find("input").val("");
+                                $(elmPopup).find("select").val("");
+                                this.closeModal();
+                            }
+                        }],
+                        closed: function () {
                         }
-                    }],
-                    closed: function () {
-                    }
-                };
-                
-                modal(option, $(elmPopup));
-                $(elmPopup).find("input[name=entity_id]").val(employee.entity_id);
-                $(elmPopup).find("input[name=full_name]").val(employee.full_name);
-                $(elmPopup).find("input[name=email]").val(employee.email);
-                $(elmPopup).find("input[name=dob]").val(employee.dob);
-                $(elmPopup).find("input[name=salary]").val(employee.salary);
-                $(elmPopup).find("select[name=department]").val(employee.department);
-                $(elmPopup).find("select[name=position]").val(employee.position);
-                $(elmPopup).modal("openModal");
-            },
+                    };
 
-            pagination: function(input) {
-                self.InputArray = input;
-                self.PageTotal = ko.computed(function() {
-                    var totalPages = Math.ceil(self.InputArray().length / parseInt(self.PerPage()));
-                    if (self.Page() > totalPages && totalPages > 0) {
-                      self.Page(totalPages);
-                    }
-                    return totalPages
-                  })
-                
-                  self.PaginatedArray = ko.computed(function() {
-                    var perPage = parseInt(self.PerPage())
-                    var start = self.Page() * perPage - perPage;
-                    return self.InputArray().slice(start, start + perPage);
-                  })
-                
-                  self.ChangePageBy = function(offset) {
-                    return function() {
-                      self.Page(self.Page() + offset);
-                    }
-                  }
-                
-                  self.ChangePageTo = function(newPage) {
-                    return function() {
-                      switch (newPage) {
-                        case "last":
-                          self.Page(self.PageTotal());
-                          break;
-                        case "first":
-                          self.Page(1);
-                          break;
-                        default:
-                          self.Page(newPage);
-                      }
-                    }
+                    modal(option, $(elmPopup));
+                    $(elmPopup).find("input[name=entity_id]").val(employee.entity_id);
+                    $(elmPopup).find("input[name=full_name]").val(employee.full_name);
+                    $(elmPopup).find("input[name=email]").val(employee.email);
+                    $(elmPopup).find("input[name=dob]").val(employee.dob);
+                    $(elmPopup).find("input[name=salary]").val(employee.salary);
+                    $(elmPopup).find("select[name=department]").val(employee.department);
+                    $(elmPopup).find("select[name=position]").val(employee.position);
+                    $(elmPopup).modal("openModal");
                 }
-            }
+
+            },
         });
     }
 );
